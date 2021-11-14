ded152173f61b1ea6ac093d00510c2a206ec25bc
rhoda Mumo <rhodamumo@gmail.com>
2021-11-14 19:52:15 +0300

    second commit

---
+++ app/src/main/java/era/com/FragmentHomeConnection.kt
@@ -0,0 +1,78 @@
+package era.com
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+
+class FragmentHomeConnection : AppCompatActivity() {
+    private val fragmentManger = supportFragmentManager
+    private val homeFrag= Homepage()
+    private val healthFrag= healthtips()
+    private val notificationFrag= Notifications()
+    private val settingsFrag= Settingpage()
+    /**call id*/
+    private lateinit var btnHo : ImageButton
+    private lateinit var btnHe : ImageButton
+    private lateinit var btnN : ImageButton
+    private lateinit var btnS : ImageButton
+    private lateinit var manger :FragmentTransaction
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_home_connection)
+        /**set find id view*/
+        btnHo= findViewById(R.id.home)
+        btnHe= findViewById(R.id.healthtips)
+        btnN= findViewById(R.id.notications)
+        btnS= findViewById(R.id.settings)
+        /**set First Fragment*/
+        manger = fragmentManger.beginTransaction()
+            .replace(R.id.myView,homeFrag)
+        manger.commit()
+        btnHo.setImageResource(R.drawable.ic_baseline_home_24)
+    }
+
+    fun addFragOnClick(view: View){
+        /**set replace fragment*/
+        manger = fragmentManger.beginTransaction()
+        when(view){
+            btnHo -> {
+                manger.replace(R.id.myView,homeFrag)
+                    .commit()
+                btnHo.setImageResource(R.drawable.ic_baseline_home_24)
+                btnHe.setImageResource(R.drawable.ic_dashboard_black_24dp)
+                btnN.setImageResource(R.drawable.ic_notifications_black_24dp)
+                btnS.setImageResource(R.drawable.blacksettings)
+
+            }
+            btnHe -> {
+                manger.replace(R.id.myView,healthFrag)
+                    .commit()
+                btnHo.setImageResource(R.drawable.ic_home_black_24dp)
+                btnHe.setImageResource(R.drawable.ic_baseline_dashboard_24)
+                btnN.setImageResource(R.drawable.ic_notifications_black_24dp)
+                btnS.setImageResource(R.drawable.blacksettings)
+
+            }
+            btnN -> {
+                manger.replace(R.id.myView,notificationFrag)
+                    .commit()
+                btnHo.setImageResource(R.drawable.ic_home_black_24dp)
+                btnHe.setImageResource(R.drawable.ic_dashboard_black_24dp)
+                btnN.setImageResource(R.drawable.ic_baseline_pinknotifications)
+                btnS.setImageResource(R.drawable.blacksettings)
+
+            }
+            btnN -> {
+                manger.replace(R.id.myView,notificationFrag)
+                    .commit()
+                btnHo.setImageResource(R.drawable.ic_home_black_24dp)
+                btnHe.setImageResource(R.drawable.ic_dashboard_black_24dp)
+                btnN.setImageResource(R.drawable.ic_notifications_black_24dp)
+                btnS.setImageResource(R.drawable.pinksettings)
+
+            }
+        }
+    }
+}
--- app/src/main/java/era/com/MainActivity.kt
+++ app/src/main/java/era/com/MainActivity.kt
@@ -1,8 +1,12 @@
 package era.com
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +20,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setOnboardingItems()
+
+        val getstarted = findViewById<Button>(R.id.buttonGetStarted)
+
+        getstarted.setOnClickListener {
+            val Intent = Intent(this,Registration::class.java)
+            startActivity(Intent)
+        }
 
     }
     private fun setOnboardingItems() {
